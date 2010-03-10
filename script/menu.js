d556da8e62d6119d705851221e179fea68e67ab5
Your Name <amorim@timedev.com>
2010-03-10 15:11:15 -0300

    Implementing special menu with jquery

---
+++ script/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("[id|=sub]").hide();
+	$("a[id|=opt]").click(function(){
+		$("[id|=sub]").hide();
+		$("a[id|=opt]").each(function(){
+			$("#" + this.id).removeClass().addClass("inicial");
+		})
+		$("#" + this.id).removeClass().addClass("selecionado");
+		$("#sub-" + this.id.replace("opt-", "")).slideDown("fast");
+		return false;
+	})
+})
